f21ec62ebfc68a1a51c8aef0a1ee51553665abb4
Per Høgfeldt <hogefeldt38@gmail.com>
2019-03-13 18:43:04 +0100

    I2C code has been commented and prettyfied

--- lab2/exercise1/Design01.cydsn/main.c
+++ lab2/exercise1/Design01.cydsn/main.c
@@ -11,10 +11,15 @@
 */
 #include "project.h"
 #include <math.h>
-// The next section of code borrowed from the url: 
+
+#define FIRST_LM75 72
+#define SECOND_LM75 79
+#define DELAY 1000
+#define BUFFER_SIZE 256
+
+// The next section of code is borrowed from the url: 
 // https://stackoverflow.com/questions/2689028/need-fastest-way-to-convert-2s-complement-to-decimal-in-c?rq=1
 // =====================================================================
-
 //I nedenstående kode udregnes to kompliment
 int binTwosComplementToSignedDecimal(char binary[],int significantBits) 
 {
@@ -37,16 +42,17 @@ int binTwosComplementToSignedDecimal(char binary[],int significantBits)
 
     return sum;
 }
+//=====================================================================
 
 void readTemp(uint8 addr) {
     uint8 temp;                     //bruges til hel-tal
     uint8 temphalf;                 //bruges til halve, f.eks. 0."5"
     uint8 status;                   //bruges til at fejlfinde
-    char buffer[256];               //bruges til at konvertere modtaget data over i 
+    char buffer[BUFFER_SIZE];       //bruges til at konvertere modtaget data over i 
     
+    // Prepare master to send
     status = I2C_1_MasterSendStart(addr,1);
-    
-    if(status == I2C_1_MSTR_NO_ERROR){                      //tjekkes om der er fejl
+    if(status == I2C_1_MSTR_NO_ERROR){      //tjekkes om der er fejl
         
         //læser første og anden byte, og skriver tilbage med hhv. ACK og NAK
         temp = I2C_1_MasterReadByte(I2C_1_ACK_DATA);        
@@ -54,12 +60,13 @@ void readTemp(uint8 addr) {
         
         I2C_1_MasterSendStop(); //stopper kommunikationen
         
+        // Konverter 2-complement til decimal
         int realTemp = binTwosComplementToSignedDecimal(buffer,8);
       
+        // test om temperaturen er x,0 eller x,5
         uint8 half = (temphalf & 0b10000000) == 0b10000000; 
         if(half) {
         snprintf(buffer, sizeof(buffer),"Read from %d: %d,5\r\n", addr, temp);
-          
         } else {
         snprintf(buffer, sizeof(buffer),"Read from %d: %d,0\r\n", addr, temp);
         }
@@ -75,17 +82,15 @@ int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
     UART_1_PutString("Program starts \n");
+    // Initalize I2C and UART
     I2C_1_Start();
-    
     UART_1_Start();
-    
+   
     for(;;)
     {
-        readTemp(72);
-        CyDelay(1000);
-        readTemp(79);
-        CyDelay(1000);
+        readTemp(FIRST_LM75);
+        CyDelay(DELAY);
+        readTemp(SECOND_LM75);
+        CyDelay(DELAY);
     }
 }
-
-/* [] END OF FILE */
